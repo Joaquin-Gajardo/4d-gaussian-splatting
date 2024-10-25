969e3e4ba59180514bd2a8a71d49f3e6eb81cd3f
Joaquin-Gajardo <joaquin.gajardocastillo@gmail.com>
2024-10-25 19:07:46 +0000

    hot fix for render.py

--- render.py
+++ render.py
@@ -14,6 +14,8 @@ from scene import Scene
 import os
 from tqdm import tqdm
 from os import makedirs
+from omegaconf import OmegaConf
+from omegaconf.dictconfig import DictConfig
 from gaussian_renderer import render
 import torchvision
 from utils.general_utils import safe_state
@@ -34,10 +36,10 @@ def render_set(model_path, name, iteration, views, gaussians, pipeline, backgrou
         torchvision.utils.save_image(rendering, os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
         torchvision.utils.save_image(gt, os.path.join(gts_path, '{0:05d}'.format(idx) + ".png"))
 
-def render_sets(dataset : ModelParams, iteration : int, pipeline : PipelineParams, skip_train : bool, skip_test : bool):
+def render_sets(dataset : ModelParams, pipeline : PipelineParams, skip_train : bool, skip_test : bool, gaussian_dim: int, time_duration: list, rot_4d: bool, force_sh_3d: bool, eval_shfs_4d: bool):
     with torch.no_grad():
-        gaussians = GaussianModel(dataset.sh_degree, gaussian_dim=4, rot_4d=True)
-        scene = Scene(dataset, gaussians, shuffle=False)
+        gaussians = GaussianModel(dataset.sh_degree, gaussian_dim=gaussian_dim, time_duration=time_duration, rot_4d=rot_4d, force_sh_3d=force_sh_3d, sh_degree_t=2 if eval_shfs_4d else 0)
+        scene = Scene(dataset, gaussians, shuffle=False, time_duration=time_duration)
 
         bg_color = [1,1,1] if dataset.white_background else [0, 0, 0]
         background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
@@ -53,14 +55,39 @@ if __name__ == "__main__":
     parser = ArgumentParser(description="Testing script parameters")
     model = ModelParams(parser, sentinel=True)
     pipeline = PipelineParams(parser)
+    parser.add_argument("--config", type=str, default="config.yaml")
     parser.add_argument("--iteration", default=-1, type=int)
     parser.add_argument("--skip_train", action="store_true")
     parser.add_argument("--skip_test", action="store_true")
     parser.add_argument("--quiet", action="store_true")
     args = get_combined_args(parser)
-    print("Rendering " + args.model_path)
 
+    # Get config from training
+    cfg_path = os.path.join(args.model_path, args.config)
+    cfg = OmegaConf.load(cfg_path)
+    def recursive_merge(key, host):
+        # Skip loaded_pth to preserve CL argument
+        if key == 'loaded_pth':
+            return
+        if isinstance(host[key], DictConfig):
+            for key1 in host[key].keys():
+                recursive_merge(key1, host[key])
+        else:
+            #assert hasattr(args, key), key
+            setattr(args, key, host[key])
+    for k in cfg.keys():
+        recursive_merge(k, cfg)
+
+    # Check if data folder exists (e.g if we're rendering at a different host from where the model was trained and the data path is different)
+    if not os.path.exists(args.source_path):
+        short_path = '/'.join(args.source_path.split('/')[-3:])
+        if not os.path.exists(short_path):
+            raise FileNotFoundError(f"Could not find path {args.source_path} or {short_path}")
+        args.source_path = short_path
+
+    print("Rendering " + args.model_path)
+    
     # Initialize system state (RNG)
     safe_state(args.quiet)
 
-    render_sets(model.extract(args), args.iteration, pipeline.extract(args), args.skip_train, args.skip_test)
+    render_sets(model.extract(args), args.iteration, pipeline.extract(args), args.skip_train, args.skip_test, args.gaussian_dim, args.time_duration, args.rot_4d, args.force_sh_3d, args.eval_shfs_4d)
